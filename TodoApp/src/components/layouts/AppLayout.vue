0c5e2e089f451c1c436b5696c25f93ffe8b1798e
devfarhad <farhad03009776700@gmail.com>
2022-12-02 21:17:18 +0500

    Todo app under development commit 1

--- TodoApp/src/components/layouts/AppLayout.vue
+++ TodoApp/src/components/layouts/AppLayout.vue
@@ -1,13 +1,18 @@
 <script setup>
-defineProps({
-});
+import loginForm from "../login/loginForm.vue";
+
+defineProps({});
 </script>
 
 <template>
-  <div>
-    <p> Basic App layout</p>
+  <div class="bg-gray-50 app-container">
+    <loginForm class="w-full h-full" />
   </div>
 </template>
 
 <style scoped>
+.app-container {
+    width: 100vw;
+    height: 100vh;
+}
 </style>
